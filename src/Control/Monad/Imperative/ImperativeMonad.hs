06f25c1ce83ec2b4f1ff61a2db1f504b1632e88f
Matthew Mirman <titaniumeyes@aol.com>
2012-04-16 01:40:55 -0400

    added the GNUv3 license, and clause at the beginning of each file

--- src/Control/Monad/Imperative/ImperativeMonad.hs
+++ src/Control/Monad/Imperative/ImperativeMonad.hs
@@ -9,8 +9,23 @@
 -- Author      :  Matthew Mirman ( mmirman@andrew.cmu.edu )
 -- Stability   :  experimental
 -- Portability :  portable
+-- Description :  A module for Imperative haskell code.
+-- License     :  GNUv3
 -- 
--- A module for Imperative haskell code.
+-- Copyright (C) 2012  Matthew Mirman
+-- 
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 module Control.Monad.Imperative.ImperativeMonad 
        ( modifyOp
--- src/Main.hs
+++ src/Main.hs
@@ -8,8 +8,23 @@
 -- Author      :  Matthew Mirman ( mmirman@andrew.cmu.edu )
 -- Stability   :  experimental
 -- Portability :  portable
+-- Description :  A test/example module for Control.Monad.Imperative
+-- License     :  GNUv3
 -- 
--- A test/example module for Control.Monad.Imperative
+-- Copyright (C) 2012  Matthew Mirman
+-- 
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 module Main where
 
