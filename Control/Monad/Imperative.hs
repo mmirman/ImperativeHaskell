6e0825817121f8ce2c24adb505367fc6417e88c2
Matthew Mirman <titaniumeyes@aol.com>
2012-04-20 21:31:52 -0400

    Updated the readme to point to the relevant blog post

--- Control/Monad/Imperative.hs
+++ Control/Monad/Imperative.hs
@@ -1,27 +1,13 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Monad.Imperative
--- Author      :  Matthew Mirman ( mmirman@andrew.cmu.edu )
+-- Maintainer  :  Matthew Mirman <mmirman@andrew.cmu.edu>
 -- Stability   :  experimental
 -- Portability :  portable
 -- Description :  A front end for the ImperativeMonad
 -- License     :  GNUv3
 -- 
--- Copyright (C) 2012  Matthew Mirman
--- 
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
--- 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
--- 
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>
-
+-----------------------------------------------------------------------------
 module Control.Monad.Imperative (module X) where
 
 import Control.Monad.Imperative.ImperativeMonad as X
