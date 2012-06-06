d897e4c5b911dde0b977e5c2339d2622039ca6f7
Matthew Mirman <titaniumeyes@aol.com>
2012-06-05 19:44:16 -0700

    fixed internals.  need to fix operators

--- Control/Monad/Imperative/Internals.hs
+++ Control/Monad/Imperative/Internals.hs
@@ -3,7 +3,13 @@
  EmptyDataDecls,
  GeneralizedNewtypeDeriving,
  MultiParamTypeClasses,
- FlexibleInstances
+ FlexibleInstances,
+ ExistentialQuantification,
+ FlexibleContexts,
+ UndecidableInstances,
+ TypeFamilies,
+ ScopedTypeVariables,
+ FunctionalDependencies 
  #-}
 
 -----------------------------------------------------------------------------
@@ -25,14 +31,11 @@ module Control.Monad.Imperative.Internals
        , break'
        , continue'
        , return'
-       , returnV
-       , returnF
        , function 
        , new
        , auto
        , runImperative
        , V(Lit, C)       
-       , ValTp
        , MIO()
        , Comp
        , Val
@@ -40,66 +43,113 @@ module Control.Monad.Imperative.Internals
        , (=:)
        , (&)
        , val
-       ) where
+       )  where
 
 import Control.Monad.Cont
 import Control.Monad.Reader
 import Data.IORef
 import Data.String (IsString(..))
 
-newtype MIO r a = MIO { getMIO :: ReaderT (Control r) (ContT r IO) a }
-                deriving (Monad, MonadCont, MonadIO)
+data InLoop
+data InFunction
 
 data Var
 data Val
-data Comp
+data Comp i b
 
-class ValTp b where
-instance ValTp Var where
-instance ValTp Val where
-instance ValTp Comp where
-
-data Control r = InFunction (r -> ContT r IO ()) 
-               | InLoop { controlBreak :: MIO r () 
-                        , controlContinue :: MIO r () 
-                        , controlReturn :: r -> MIO r ()
-                        }
-
--- | @'returnF' value@ acts like the imperative return, where
--- if called, it will exit the current function and place the 
--- returned value into the current continuation.  Note, this
--- doesn't work inside of loops.  Inside of loops, we need
--- 'returnV'
-returnF :: ValTp a => V a b b -> MIO b b
-returnF v = MIO $ do
-  v' <- getMIO $ val v
-  a <- ask
-  case a of
-    InLoop _ _ ret -> getMIO $ ret v'
-    InFunction ret -> lift $ ret v'
-  return v'
+newtype MIO i r a = MIO { getMIO :: ReaderT (Control i r) (ContT r IO) a }
+                  deriving (Monad, MonadCont, MonadIO)
   
--- | @'returnV' value@ acts like the imperative return, where
--- if called, it will exit the current function and place the 
--- returned value into the current continuation.  Note, this
--- doesn't work as a last function call.
-returnV :: ValTp a => V a b b -> MIO b ()
-returnV a = returnF a >> return ()
+data V b r a where
+  R :: IORef a -> V Var r a
+  Lit :: a -> V Val r a
+  C :: MIO i r (V b r a) -> V (Comp i b) r a
+  
+data Control i r where  
+  InFunction :: (r -> ContT r IO ()) -> Control InFunction r
+  InLoop :: MIO InLoop r () -> MIO InLoop r () -> (r -> ContT r IO ()) -> Control i r
 
-class Returnable b r where
-  -- | @'return''@ can act as returnF or returnV depending on use
-  -- if it does not work, it is likely that type inference
-  -- could not figure out a sensible alternative.
-  return' :: ValTp a => V a b b -> MIO b r
+getReturn :: Control i r -> r -> ContT r IO ()
+getReturn (InFunction ret) = ret
+getReturn (InLoop _ _ ret) = ret
 
-instance Returnable b () where
-  return' a = returnV a 
+mioLoop :: ReaderT (Control InLoop r) (ContT r IO) a -> MIO InLoop r a
+mioLoop = MIO 
 
-instance Returnable b b where
-  return' a = returnF a
+mioFunc :: ReaderT (Control InFunction r) (ContT r IO) a -> MIO InFunction r a
+mioFunc = MIO
+
+class HasValue b i where
+  val :: V b r a -> MIO i r a
+instance HasValue Var i where
+  val (R r) = MIO $ liftIO $ readIORef r
+instance HasValue Val i where
+  val (Lit v) = return v
+instance HasValue b InLoop => HasValue (Comp InLoop b) InLoop where
+  val (C m) = val =<< m
+instance HasValue b InLoop => HasValue (Comp InFunction b) InLoop where
+  val (C m) = val =<< toLoop m
+instance HasValue b InFunction => HasValue (Comp InFunction b) InFunction where
+  val (C m) = val =<< m
 
 
-runImperative :: MIO a a -> IO a
+class State i where 
+  getState :: MIO i r (Control i r)
+  
+  type RetTy i r 
+  return' :: r -> MIO i r (RetTy i r)
+  
+  toLoop :: MIO i r a -> MIO InLoop r a  
+  
+instance State InFunction where 
+  getState = mioFunc ask
+  
+  type RetTy InFunction a = a
+  return' r = mioFunc $ do
+    InFunction ret <- ask
+    lift $ ret r
+    return r
+
+  toLoop (MIO m) = MIO $
+    withReaderT (\(InLoop _ _ retLoop) -> InFunction retLoop) m
+
+  
+instance State InLoop     where 
+  getState = mioLoop ask
+
+  type RetTy InLoop a = ()
+  return' r = mioLoop $ do
+    InLoop _ _ ret <- ask
+    lift $ ret r
+    return ()
+    
+  toLoop m = m  
+  
+for' :: (State i, HasValue b i, HasValue b InLoop) => (MIO i r irr1, V b r Bool, MIO i r irr2) -> MIO InLoop r () -> MIO i r ()
+for' (init, check, incr) (body :: MIO InLoop r ()) = do init 
+                                                        for_r
+    where for_r = do
+            do_comp <- val check
+            when do_comp $ callCC $ \break_foo -> do
+                           callCC $ \continue_foo ->
+                             MIO $ flip withReaderT (getMIO body) $ \inbod -> 
+                               InLoop (toLoop $ break_foo ()) (toLoop $ continue_foo ()) (getReturn inbod)
+                           incr
+                           for_r
+    
+
+break' :: MIO InLoop r ()
+break' = do
+  InLoop b _ _ <- getState
+  b
+
+continue' :: MIO InLoop r ()
+continue' = do
+  InLoop _ c _ <- getState
+  c
+
+
+runImperative :: MIO InFunction a a -> IO a
 runImperative foo = do
   a <- runContT (callCC $ \ret -> runReaderT (getMIO foo) $ InFunction ret) return
   return a
@@ -107,28 +157,13 @@ runImperative foo = do
 -- | @'function' foo@ takes an ImperativeMonad action and removes it from it's  
 -- specific function context, specifically making it applicable 
 -- in the body of other functions.
-function :: MIO a a -> MIO b a
+function :: MIO InFunction a a -> MIO i b a
 function = MIO . liftIO . runImperative
 
--- | @'break''@ exists the current loop.
--- if called outside of a loop, rather than throwing a compilation error,
--- it will simply return a runtime error.
-break' :: MIO a ()
-break' = MIO ask >>= controlBreak
-
--- | 'continue'' continues the current loop, passing over
--- any control flow that is defined.
--- if called outside of a loop, rather than throwing a compilation error,
--- it will simply return a runtime error.
-continue' :: MIO a ()
-continue' = MIO ask >>= controlContinue
 
 
 
-data V b r a where
-  R :: IORef a -> V Var r a
-  Lit :: a -> V Val r a
-  C :: ValTp b => MIO r (V b r a) -> V Comp r a
+
 instance Eq a => Eq (V Val r a) where
   (Lit a) == (Lit a') = a == a'
 instance Show a => Show (V Val r a) where
@@ -139,20 +174,14 @@ instance Num a => Num (V Val r a) where
   abs (Lit a) = Lit $ abs a
   signum (Lit a) = Lit $ signum a
   fromInteger = Lit . fromInteger
-  
 instance IsString s => IsString (V Val r s) where
   fromString = Lit . fromString
 
 
-val :: ValTp b => V b r a -> MIO r a
-val v = case v of
-  R r -> MIO $ liftIO $ readIORef r
-  Lit v -> return v
-  C m -> val =<< m
-
 -- | @('&')a@ gets a reference/pointer to the variable specified
 (&) :: V Var r a -> V Var s a
 (&) (R a) = R a
+
 
 -- | 'auto' should just be used where the 
 -- type can be automatically infered and we don't need an initial value
@@ -160,7 +189,7 @@ val v = case v of
 auto = undefined
 
 -- | 'new' constructs a new reference to the specified pure value
-new :: a -> MIO r (V Var r a)
+new :: a -> MIO i r (V Var r a)
 new a = do
   r <- MIO $ liftIO $ newIORef a
   return $ R r
@@ -171,41 +200,28 @@ infixr 0 =:
 -- | The 'Assignable' class is used to specify a value which can be 
 -- computed imperatively.
 
-class Assignable valt where 
+class Assignable i valt where 
   -- | @variable '=:' value@ executes @value@ and writes it  
   -- to the location pointed to by @variable@
-  (=:) :: V Var r a -> valt r a -> MIO r () 
+  (=:) :: V Var r a -> valt r a -> MIO i r () 
 
-instance ValTp b => Assignable (V b) where  
+instance (HasValue b i) => Assignable i (V b) where  
   (=:) (R ar) br = MIO $ do
     b <- getMIO $ val br
     liftIO $ writeIORef ar b
 
-instance Assignable MIO where  
+instance Assignable i (MIO i) where  
   (=:) (R ar) br = do
     b <- br
     liftIO $ writeIORef ar b
 
-
--- | @'for''(init, check, incr)@ acts like its imperative @for@ counterpart
-for' :: ValTp b => (MIO r irr1, V b r Bool, MIO r irr2) -> MIO r () -> MIO r ()
-for' (init, check, incr) body = init >> for_r
-  where for_r = do
-          do_comp <- val check
-          when do_comp $ callCC $ \break_foo -> do
-                         callCC $ \continue_foo -> MIO $ do
-                           flip withReaderT (getMIO body) $ \inbod ->
-                             InLoop (break_foo ()) (continue_foo ()) (controlReturn inbod)
-                         incr
-                         for_r
-
 -- | @'while''(check)@ acts like its imperative @while@ counterpart.
-while' :: ValTp b => V b r Bool -> MIO r () -> MIO r ()
+while' :: (HasValue b i, HasValue b InLoop, State i) => V b r Bool -> MIO InLoop r () -> MIO i r ()
 while' check = for'(return (), check, return () )
 
 -- | @'if''(check) act@ only performs @act@ if @check@ evaluates to true
 -- it is specifically a value in its argument.
-if' :: ValTp b => V b r Bool -> MIO r () -> MIO r ()
+if' :: (HasValue b i) => V b r Bool -> MIO i r () -> MIO i r ()
 if' b m = do
   v <- val b
   when v m
@@ -214,7 +230,7 @@ if' b m = do
 -- out of a binary haskell function.
 -- The suggested use is to replicate functionality of assignments
 -- like @-=@ or @%=@ from C style languages.
-modifyOp :: ValTp k => (a->b->a) -> V Var r a -> V k r b -> MIO r ()
+modifyOp :: (HasValue k i) => (a->b->a) -> V Var r a -> V k r b -> MIO i r ()
 modifyOp op (R ar) br = MIO $ do
   b <- getMIO $ val br
   liftIO $ modifyIORef ar (\v -> op v b)
